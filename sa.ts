1674a1a209fce827a6cf29378c4e4ab583f1a8cb
jayden3425-gif <jdd380046@outlook.com>
2025-01-28 16:11:38 +0000

    Initial files for MakeCode project

---
+++ sa.ts
@@ -0,0 +1,28 @@
+// Add your code here
+//% weight=100 color=#0fbc11 icon="\uf11b"
+namespace advanced {
+
+    /**
+     * Flips the sprite horizontally
+     */
+    //% group="Advanced"
+    //% blockId=sprites_advanced_flip_horizontally block="flip %sprite horizontally"
+    //% blockNamespace=sprites
+    //% sprite.defl=mySprite
+    //% sprite.shadow=variables_get
+    export function flipHorizontally(sprite: Sprite) {
+        sprite.image.flipX();
+    }
+
+    /**
+     * Flips the sprite vertically
+     */
+    //% group="Advanced"
+    //% blockId=sprites_advanced_flip_vertically block="flip %sprite vertically"
+    //% blockNamespace=sprites
+    //% sprite.defl=mySprite
+    //% sprite.shadow=variables_get
+    export function flipVertically(sprite: Sprite) {
+        sprite.image.flipY();
+    }
+}
